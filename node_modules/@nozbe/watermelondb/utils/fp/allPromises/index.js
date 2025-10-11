b935c8b3935240da59abcf5f6b8086cb8dc16381
Kush Honkalse <kush.honkalse@gmail.com>
2025-10-11 12:21:10 +0530

    fixes

--- node_modules/@nozbe/watermelondb/utils/fp/allPromises/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-exports.default = void 0;
-var allPromises = function (action, promises) {
-  return Promise.all(promises.map(action));
-};
-var _default = exports.default = allPromises;
